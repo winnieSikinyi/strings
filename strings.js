1bae5ebd769af5d7b2dceefecbc1a858dc45af6a
winnieSikinyi <winniesikinyi@gmail.com>
2023-03-10 20:03:23 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,25 @@
+let text = "beautiful"
+
+console.log(text[0]);
+console.log(text[1]);
+console.log(text[2]);
+console.log(text[3]);
+//Insert the string at the tenth index of the variable const food = idid not have appetite today
+const food = "I did not have appetite today"
+var word= "eat"
+
+console.log(`${food [10] + word}`)
+//Count how many times the following string appears in the string variable:
+//1. "the"
+//2."s"
+const story= "She sells sea shells at the sea shore"
+var char="the"
+var count=story.split(char).length-1
+
+
+//Convert the following strings into the specified format:
+//1. UpperCase: "CONfidant"
+//2. LowerCase: "amazing", "beautiFUL"
+//3. Title case "A busy office"
+let statement1 =  "CONfidant"
+console.log(statement1.toUpperCase());
